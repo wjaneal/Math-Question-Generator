426a8c355f2b989833822ceaf56a8701c95269eb
William Neal <wneal@lia-edu.ca>
2013-05-10 11:13:22 -0300

    Create vectors.py
    Classes for Use of Vectors in the Question Generator Program.

---
+++ vectors.py
@@ -0,0 +1,57 @@
+from base_classes import *
+from visual import *
+
+#Define Vector Magnitude Questions
+def Vector_Magnitude():
+  Range1 = NumberRange(-10,10)
+	EquationString = "\\vec{a} = [*,*,*]"
+	NumVariables = CountStars(EquationString)
+	RangeSet = NumVariables*[Range1]
+	VariableSet = NumVariables*["Integer"]
+	Q1 = QuestionForm(EquationString, VariableSet,RangeSet)
+	QuestionString = Q1.Create_Equation_String(Q1.Format_String,0)
+	Answer = mag(vector(float(Q1.Variables[0]),float(Q1.Variables[1]),float(Q1.Variables[2])))
+	AnswerString = "\\lvert\\vec{a}\\rvert \\approx "+"%.2f" % (Answer) + ""
+	AnswerString = MakeLatexEquation(AnswerString)
+	QuestionString = MakeLatexEquation(QuestionString)
+	return [QuestionString, AnswerString]
+
+#Define Dot Product Questions
+def Dot_Product():
+	Range1=NumberRange(-10,10)
+	EquationString = "\\vec{a} = [*,*,*]\\\\\\vec{b} = [*,*,*]\\\\\\vec{a} \cdot \\vec{b} = "
+	NumVariables = CountStars(EquationString)
+	RangeSet = NumVariables*[Range1]
+	VariableSet = NumVariables*["Integer"]
+	Q1 = QuestionForm(EquationString, VariableSet, RangeSet)
+	QuestionString = Q1.Create_Equation_String(Q1.Format_String,0)
+	A = vector(0.0,0.0,0.0)
+	B = vector(0.0,0.0,0.0)
+	for i in range(0,3):
+		A[i] = float(Q1.Variables[i])
+		B[i] = float(Q1.Variables[i+3])
+	Answer = dot(A,B)
+	AnswerString = "\\vec{a} \cdot \\vec{b} = "+str(Answer)
+	AnswerString = MakeLatexEquation(AnswerString)
+	QuestionString = MakeLatexEquation(QuestionString)
+	return [QuestionString, AnswerString]
+
+#Define Cross Product Questions
+def Cross_Product():
+	Range1=NumberRange(-10,10)
+	EquationString = "\\vec{a} = [*,*,*]\\\\\\vec{b} = [*,*,*]\\\\\\vec{a} \\times \\vec{b} = "
+	NumVariables = CountStars(EquationString)
+	RangeSet = NumVariables*[Range1]
+	VariableSet = NumVariables*["Integer"]
+	Q1 = QuestionForm(EquationString, VariableSet, RangeSet)
+	QuestionString = Q1.Create_Equation_String(Q1.Format_String,0)
+	A = vector(0.0,0.0,0.0)
+	B = vector(0.0,0.0,0.0)
+	for i in range(0,3):
+		A[i] = float(Q1.Variables[i])
+		B[i] = float(Q1.Variables[i+3])
+	Answer = cross(A,B)
+	AnswerString = "\\vec{a} \\times \\vec{b} = ["+str(Answer[0])+","+str(Answer[1])+","+str(Answer[2])+"]"
+	AnswerString = MakeLatexEquation(AnswerString)
+	QuestionString = MakeLatexEquation(QuestionString)
+	return [QuestionString, AnswerString]
